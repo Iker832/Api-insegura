c4630999aa843ad38862cdb4945b372dbe614c57
Iker832 <159393759+Iker832@users.noreply.github.com>
2025-05-28 13:29:21 -0600

    db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,30 @@
-create database webapps:
 
+use webapps;
 
-GRANT ALL PRIVILEGES ON webapps.* TO 'udbwebapps'@'localhost' IDENTIFIED BY '250402_kike';
-FLUSH PRIVILEGES
+create table if not exists Usuario(
+	id int AUTO_INCREMENT, 
+	uname varchar(50) not null, 
+	email varchar(250) not null, 
+	password varchar(250) not null,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+create table if not exists AccesoToken(
+	id_Usuario int primary key, 
+	token varchar(250) not null, 
+	fecha datetime not null
+)ENGINE=InnoDB;
+
+create table if not exists Imagen(
+	id int AUTO_INCREMENT, 
+	name varchar(250) not null, 
+	ruta text not null,
+	id_Usuario int not null,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+ALTER TABLE Usuario ADD CONSTRAINT U_U Unique (uname,email);
+
+ALTER TABLE AccesoToken ADD CONSTRAINT FK_ATU FOREIGN KEY (id_Usuario) REFERENCES Usuario(id);
+
+ALTER TABLE Imagen ADD CONSTRAINT FK_I_U FOREIGN KEY (id_Usuario) REFERENCES Usuario(id);
